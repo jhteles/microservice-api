a1158f3cf76175d6f4be92fbf9a9be9a4aec5440
joseteles <josehct@gmail.com>
2021-08-23 10:06:17 -0300

    1.1

--- product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/controller/ProductController.java
@@ -3,10 +3,9 @@ package one.digitalinnovation.experts.productcatalog.controller;
 import one.digitalinnovation.experts.productcatalog.model.Product;
 import one.digitalinnovation.experts.productcatalog.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/product")
@@ -17,8 +16,11 @@ public class ProductController {
 
     @RequestMapping(method = RequestMethod.POST)
     Product create(@RequestBody Product product){
-
         return productRepository.save(product);
+    }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    Optional<Product> findById(@PathVariable Integer id){
+        return productRepository.findById(id);
     }
 }
